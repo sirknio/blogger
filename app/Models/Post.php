b30630b1a9ec4e2176e8c4388ebd3debfe4121e0
Carlos Arboleda <knio16@hotmail.com>
2021-05-28 23:51:38 -0500

    Creación del modelo para los post

---
+++ app/Models/Post.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relación muchos a muchos
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
+
+    // relación polimorfica uno a uno (Inversa)
+    public function image()
+    {
+        return $this->morphOne(Image::class,'imageable');
+    }
+}
