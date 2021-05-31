08c5d4cf3eadbd32514cd381aa36251681a7d6cc
Carlos Arboleda <knio16@hotmail.com>
2021-05-30 21:11:44 -0500

    Vista para mostrar los posts que se hayan creado y una lista de relacionados

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,17 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::where('status', 2)->latest('id')->paginate(8);
-
         return view('posts.index', compact('posts'));
+    }
+
+    public function show(Post $post)
+    {
+        $similars = Post::where('category_id', $post->category_id)
+                            ->where('status', 2)
+                            ->where('id','!=',$post->id)
+                            ->latest('id')
+                            ->take(4)
+                            ->get();
+        return view('posts.show', compact('post', 'similars'));
     }
 }
