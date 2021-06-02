41cf75da346587b18627b1b5c2aeba185fdb1acd
Carlos Arboleda <knio16@hotmail.com>
2021-06-02 14:32:24 -0500

    implementación AdminLTE sobre le proyecto

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,20 @@
-Pruebas
+@extends('adminlte::page')
+
+@section('title', 'Admin LTE')
+
+@section('content_header')
+    <h1>Dashboard</h1>
+@stop
+
+@section('content')
+    <p>Welcome to this beautiful admin panel.</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
+ 
--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,10 @@ use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [HomeController::class, 'index']);
+Route::get('1', function () {
+    return view('admin.test1');
+});
+
+Route::get('2', function () {
+    return view('admin.test2');
+});
