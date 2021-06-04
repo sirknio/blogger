95621e033b0eea274a0e16a35a718bd50153cd45
Carlos Arboleda <knio16@hotmail.com>
2021-06-04 06:21:46 -0500

    Creación del resource Controller

--- routes/admin.php
+++ routes/admin.php
@@ -1,11 +1,12 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
-
+Route::resource('categories', CategoryController::class)->names('admin.categories');
 
 Route::get('1', function () {
     return view('admin.test1');
