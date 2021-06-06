086eac43eaeb8b8d1814dfd45eca85fd4b14b841
Carlos Arboleda <knio16@hotmail.com>
2021-06-06 14:35:02 -0500

    Paginación con Livewire y para que quede con Bootstrap

--- app/Http/Livewire/Admin/PostsIndex.php
+++ app/Http/Livewire/Admin/PostsIndex.php
@@ -4,8 +4,12 @@ namespace App\Http\Livewire\Admin;
 
 use App\Models\Post;
 use Livewire\Component;
+use Livewire\WithPagination;
+
 class PostsIndex extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = "bootstrap";
     public function render()
     {
         $posts = Post::where('user_id', auth()->user()->id)->latest('id')->paginate(10);
